b969acced6291b6e3e90ced0deb201a7c9706ced
vossgang <vossgang@gmail.com>
2014-03-29 08:28:05 -0700

    Fixed some logic issues

--- GameOfLife/MVViewController.m
+++ GameOfLife/MVViewController.m
@@ -16,7 +16,8 @@
 @property (nonatomic) NSInteger numberOfColumns;
 @property (nonatomic) NSInteger cellSize;
 @property (nonatomic, strong)  NSTimer *lifeCheck;
-
+@property (nonatomic, strong) UIView *pause;
+@property (nonatomic, strong) NSMutableArray *copiedArray;
 
 
 @end
@@ -27,12 +28,15 @@
 {
     [super viewDidLoad];
     
-    self.cellSize = 11;
+    self.cellSize = 20;
     
     CGRect screenRect = [[UIScreen mainScreen] bounds];
+    self.pause = [[UIView alloc] initWithFrame:CGRectMake(0, screenRect.size.height - 50, screenRect.size.width, 50)];
+    self.pause.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.pause];
     
     self.numberOfColumns = (screenRect.size.width / self.cellSize);
-    self.numberOfRows = (screenRect.size.height / self.cellSize);
+    self.numberOfRows = ((screenRect.size.height - 50) / self.cellSize);
     
     
     NSMutableArray *rowArray = [NSMutableArray new];
@@ -45,18 +49,10 @@
             newCell.backgroundColor = [UIColor blackColor];
             [self.view addSubview:newCell];
             [columnArray addObject:newCell];
-            
-            if (!(arc4random() % 8)) {
-                newCell.isAlive = true;
-                newCell.backgroundColor = [UIColor blueColor];
-            }
-            
         }
         [rowArray addObject:columnArray];
     }
     self.cells = rowArray;
-    
-    [self setUpTimer];
     
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -73,10 +69,25 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     for (UITouch *touch in touches) {
         CGPoint touchPoint = [touch locationInView:self.view];
-        for (Cell *cell in self.view.subviews) {
-            if (CGRectContainsPoint(cell.frame, touchPoint)) {
-                cell.isAlive = true;
-                cell.backgroundColor = [UIColor blueColor];
+        if (CGRectContainsPoint(self.pause.frame, touchPoint)) {
+            if ([self.lifeCheck isValid]) {
+                [self.lifeCheck invalidate];
+                self.pause.backgroundColor = [UIColor greenColor];;
+            }else{
+                [self setUpTimer];
+                self.pause.backgroundColor = [UIColor redColor];
+                }
+        } else {
+            for (Cell *cell in self.view.subviews) {
+                if (CGRectContainsPoint(cell.frame, touchPoint)) {
+                    if (cell.isAlive) {
+                        cell.isAlive = false;
+                        cell.backgroundColor = [UIColor blackColor];
+                    } else {
+                        cell.isAlive = true;
+                        cell.backgroundColor = [UIColor blueColor];
+                    }
+                }
             }
         }
     }
@@ -87,10 +98,16 @@
 - (void)checkForLife
 {
     
-    for (int row = 0; row < _numberOfRows - 1; row++) {
+    NSMutableArray *rowArray = [NSMutableArray new];
+    for (int row = 0; row < _numberOfRows; row++) {
+        NSMutableArray *columnArray = [NSMutableArray new];
         for (int column = 0; column < _numberOfColumns; column++) {
             int numberOfLiveNeighbors = 0;
-            Cell *thisCell = self.cells[row][column];
+            Cell *thisCell = [[Cell alloc] initWithFrame:CGRectMake((column * self.cellSize), (row * self.cellSize), self.cellSize, self.cellSize)];
+            thisCell.layer.borderWidth = 1;
+            thisCell.layer.borderColor = [[UIColor darkGrayColor] CGColor];
+            thisCell.backgroundColor = [UIColor blackColor];
+            
 
             if ((column > 0) && (row > 0) && (column < (_numberOfColumns - 2)) && (row < (_numberOfRows - 2))) {
 
@@ -98,22 +115,22 @@
                 if ([self.cells[row - 1][column - 1] isAlive]) {
                     numberOfLiveNeighbors++;
                 }
-                if ([self.cells[row][column - 1] isAlive]) {
-                    numberOfLiveNeighbors++;
-                }
-                if ([self.cells[row + 1][column - 1] isAlive]) {
-                    numberOfLiveNeighbors++;
-                }
                 if ([self.cells[row - 1][column] isAlive]) {
-                    numberOfLiveNeighbors++;
-                }
-                if ([self.cells[row + 1][column] isAlive]) {
                     numberOfLiveNeighbors++;
                 }
                 if ([self.cells[row - 1][column + 1] isAlive]) {
                     numberOfLiveNeighbors++;
                 }
+                if ([self.cells[row][column - 1] isAlive]) {
+                    numberOfLiveNeighbors++;
+                }
                 if ([self.cells[row][column + 1] isAlive]) {
+                    numberOfLiveNeighbors++;
+                }
+                if ([self.cells[row + 1][column - 1] isAlive]) {
+                    numberOfLiveNeighbors++;
+                }
+                if ([self.cells[row + 1][column] isAlive]) {
                     numberOfLiveNeighbors++;
                 }
                 if ([self.cells[row + 1][column + 1] isAlive]) {
@@ -122,6 +139,9 @@
                 
                 switch (numberOfLiveNeighbors) {
                     case 2:
+                        if ([_cells[row][column] isAlive]) {
+                            thisCell.isAlive = true;
+                        }
                         break;
                     case 3:
                         if (!thisCell.isAlive) {
@@ -136,20 +156,28 @@
                 
             } else {
                 thisCell.isAlive = false;
+                thisCell.backgroundColor = [UIColor blackColor];
             }
-            _cells[row][column] = thisCell;
+            [columnArray addObject:thisCell];
 
         }
+        [rowArray addObject:columnArray];
     }
+    _cells = rowArray;
     [self updateCellView];
 
 }
 
 -(void)updateCellView
 {
+    NSArray *viewsToRemove = [self.view subviews];
+    for (UIView *v in viewsToRemove) {
+        [v removeFromSuperview];
+    }
     for (int row = 0; row < _numberOfRows; row++) {
         for (int column = 0; column < _numberOfColumns; column++) {
             Cell *livingCell = _cells[row][column];
+            [self.view addSubview:livingCell];
             if (livingCell.isAlive) {
                 livingCell.backgroundColor = [UIColor blueColor];
             } else {
@@ -158,8 +186,11 @@
             _cells[row][column] = livingCell;
         }
     }
-    
+    [self.view addSubview:self.pause];
+
     
 }
+
+
 
 @end
